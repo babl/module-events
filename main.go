acea5660c72c9aedb7e4c04433795f487865ff09
Lars Kluge <l@larskluge.com>
2016-07-01 05:37:18 +0100

    Incl subscription config & exec when triggered event matches

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
+//go:generate go-bindata -nocompress subscriptions.yml
+
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
+type subscription struct {
+	Exec string            `yaml:"exec"`
+	Env  map[string]string `yaml:"env"`
+}
+
+type config map[string][]subscription
+
 func main() {
-	fmt.Println("Hi")
+	event := os.Getenv("EVENT")
+	log.Printf("Event triggered: %s\n", event)
+
+	contents, err := Asset("subscriptions.yml")
+	check(err)
+
+	var c config
+	err = yaml.Unmarshal(contents, &c)
+	check(err)
+
+	for e, subs := range c {
+		if e == event {
+			for _, sub := range subs {
+				log.Printf("EXEC babl --host queue.babl.sh --port 4445 %s %q", sub.Exec, sub.Env)
+			}
+		}
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
