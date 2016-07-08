cdc02a6198bfc4ac0a081ce963e7f91522d2b8f9
Lars Kluge <l@larskluge.com>
2016-07-08 12:57:03 +0200

    Execute all subscriptions

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ package main
 import (
 	"log"
 	"os"
+	"sync"
 
+	babl "github.com/larskluge/babl/shared"
 	"gopkg.in/yaml.v2"
 )
 
-type subscription struct {
-	Exec string            `yaml:"exec"`
-	Env  map[string]string `yaml:"env"`
+type Subscription struct {
+	Exec string   `yaml:"exec"`
+	Env  babl.Env `yaml:"env"`
 }
 
-type config map[string][]subscription
+type config map[string][]Subscription
 
 func main() {
 	event := os.Getenv("EVENT")
@@ -22,18 +24,35 @@ func main() {
 
 	contents, err := Asset("subscriptions.yml")
 	check(err)
-
 	var c config
 	err = yaml.Unmarshal(contents, &c)
 	check(err)
 
+	stdin := babl.ReadStdin()
+
+	var wg sync.WaitGroup
 	for e, subs := range c {
 		if e == event {
 			for _, sub := range subs {
-				log.Printf("EXEC babl --host queue.babl.sh --port 4445 %s %q", sub.Exec, sub.Env)
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					exec(sub.Exec, sub.Env, &stdin)
+				}()
 			}
 		}
 	}
+	wg.Wait()
+}
+
+func exec(moduleName string, env babl.Env, stdin *[]byte) {
+	log.Printf("EXEC babl --async %s %q", moduleName, env)
+	module := babl.NewModule(moduleName)
+	module.Address = "queue.babl.sh:4445"
+	module.Env = env
+	module.SetAsync(true)
+	_, _, _, err := module.Call(*stdin)
+	check(err)
 }
 
 func check(err error) {
