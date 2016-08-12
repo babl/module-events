94f58f4db3b52a6f953ba359348fc59383eb3a90
Lars Kluge <l@larskluge.com>
2016-08-12 16:00:07 +0200

    Log warning when subscriptions can not be updated

--- update_subscriptions.go
+++ update_subscriptions.go
@@ -4,13 +4,19 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 const SubscriptionsApiUrl = "https://babl.sh/api/subscriptions"
 
 func updateSubscriptions() {
+	// TODO: timeout after 10 seconds
 	resp, err := http.Get(SubscriptionsApiUrl)
-	check(err)
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Warn("Updating subscriptions failed")
+		return
+	}
 	defer resp.Body.Close()
 
 	file, err := os.Create(SubscriptionsPath)
